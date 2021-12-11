e0cc391fb11dc24c6be40e8765c81579715bb743
Miquel <miqmago@users.noreply.github.com>
2021-12-11 22:15:35 +0100

    Allow customize sda and scl pins
    Right now it's impossible to use other pins than Arduino defaults, i.e.: SDA(21), SCL(22) on framework-espressif32.
    
    It could happen that one would need to use other pins.

--- Rtc_Pcf8563.h
+++ Rtc_Pcf8563.h
@@ -125,7 +125,7 @@ extern TwoWire Wire;
 /* arduino class */
 class Rtc_Pcf8563 {
     public:
-    Rtc_Pcf8563();
+    Rtc_Pcf8563(int sdaPin = -1, int sclPin = -1);
 
     void zeroClock();  /* Zero date/time, alarm / timer, default clkout */
     void clearStatus(); /* set both status bytes to zero */
